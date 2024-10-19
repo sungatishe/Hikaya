2f6822d5d066109f7118673c508eae592e2f866a
sungatishe <210103144@stu.sdu.edu.kz>
2024-10-19 13:23:51 +0500

    Fixed bugs with indexing

--- movie-service/internal/repository/search.go
+++ movie-service/internal/repository/search.go
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/elastic/go-elasticsearch/v8"
-	"github.com/elastic/go-elasticsearch/v8/esapi"
+	"io"
 	"movie-service/internal/models"
 )
 
@@ -37,24 +37,35 @@ func NewElasticSearchRepository() (ElasticSearchRepository, error) {
 func (r *elasticSearchRepository) IndexMovie(movieID string, movieData models.Movie) error {
 	data, err := json.Marshal(movieData)
 	if err != nil {
-		return err
+		return fmt.Errorf("could not marshal movie data: %w", err)
 	}
 
-	req := esapi.IndexRequest{
-		Index:      "movies",
-		DocumentID: movieID,
-		Body:       bytes.NewReader(data),
-		Refresh:    "true",
+	fmt.Printf("Indexing movie data: 	%s\n", string(data))
+
+	res, err := r.client.Index(
+		"movies", // Не используем fmt.Sprintf, просто конкатенация строк
+		bytes.NewReader(data),
+		r.client.Index.WithDocumentID(movieID), // Здесь передаем ID документа
+		r.client.Index.WithRefresh("true"),
+	)
+
+	// Убедитесь, что res не nil перед вызовом IsError
+	if res == nil {
+		return fmt.Errorf("response is nil")
 	}
 
-	res, err := req.Do(context.Background(), r.client)
+	if res.IsError() {
+		body, _ := io.ReadAll(res.Body) // Читаем тело ответа для диагностики
+		return fmt.Errorf("error indexing movie: %s, response body: %s", res.String(), string(body))
+	}
+
 	if err != nil {
-		return err
+		return fmt.Errorf("could not index movie: %w", err)
 	}
 	defer res.Body.Close()
 
 	if res.IsError() {
-		return fmt.Errorf("Error indexing movie: %s", res.String())
+		return fmt.Errorf("error indexing movie: %s", res.String())
 	}
 
 	return nil
@@ -62,13 +73,19 @@ func (r *elasticSearchRepository) IndexMovie(movieID string, movieData models.Mo
 
 func (r *elasticSearchRepository) SearchMovies(query string) ([]map[string]interface{}, error) {
 	var buf bytes.Buffer
+
+	// Создаем JSON-запрос для поиска
 	queryStr := fmt.Sprintf(`{
 		"query": {
 			"match": {
-				"title": %s
+				"title": {
+					"query": "%s",
+					"fuzziness": "AUTO" // Добавляем нечеткий поиск, если это необходимо
+				}
 			}
 		}
 	}`, query)
+
 	buf.WriteString(queryStr)
 
 	res, err := r.client.Search(
@@ -91,6 +108,7 @@ func (r *elasticSearchRepository) SearchMovies(query string) ([]map[string]inter
 		return nil, err
 	}
 
+	// Извлекаем результаты
 	hits := rmap["hits"].(map[string]interface{})["hits"].([]interface{})
 	var results []map[string]interface{}
 	for _, hit := range hits {
